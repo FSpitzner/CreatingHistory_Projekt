f970bf3372c2676bd188b82a107602c26b2f61b0
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-11-01 17:51:32 +0100

    bubbleshooter start

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+/// 
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bubble : MonoBehaviour 
+{
+
+    #region Variable Declarations
+    // Serialized Fields
+    [SerializeField] float power = 5f;
+    // Private
+    private Rigidbody2D rb;
+    #endregion
+
+
+
+    #region Public Properties
+
+    #endregion
+
+
+
+    #region Unity Event Functions
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start () 
+	{
+        rb.AddRelativeForce(new Vector2(0, 1) * power, ForceMode2D.Impulse);
+	}
+	#endregion
+	
+	
+	
+	#region Public Functions
+	
+	#endregion
+	
+	
+	
+	#region Private Functions
+
+	#endregion
+	
+	
+	
+	#region Coroutines
+	
+	#endregion
+}
