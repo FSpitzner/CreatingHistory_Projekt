717d48ac90b935739b522a07176f052f0eee1367
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-11-04 18:55:33 +0100

    Start Animation + big progress on the Bubble Shooter

---
+++ Assets/Scripts/BubbleShooter/BallTouchInput.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTouchInput : MonoBehaviour {
+
+    public LineRenderer line;
+    public RectTransform canvasTransform;
+    public Bubble ball;
+    public GameObject ballPrefab;
+    Vector3 startPosition;
+    Vector2 direction;
+    bool inputStarted = false;
+    bool onTheWay = false;
+
+    private void Update()
+    {
+        if (ball == null)
+        {
+            onTheWay = false;
+            ball = Instantiate(ballPrefab, transform).GetComponent<Bubble>().CreateNewRandom();
+        }
+        else if (!onTheWay)
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Vector3 startpos = Input.GetTouch(0).position;
+                startpos.z = -10f;
+                startPosition = Camera.main.ScreenToWorldPoint(startpos);
+                startPosition.z = 0f;
+                line.SetPosition(0, startPosition);
+                line.SetPosition(1, startPosition);
+                Debug.Log("Touch Began at Pos: " + startPosition);
+                inputStarted = true;
+            }
+            if (inputStarted)
+            {
+                if (Input.touchCount > 0)
+                {
+                    Vector3 touchpos = Input.GetTouch(0).position;
+                    touchpos.z = -10f;
+                    //touchpos = new Vector3(touchpos.x * canvasTransform.localScale.x, touchpos.y * canvasTransform.localScale.y, touchpos.z * canvasTransform.localScale.z);
+                    Vector3 worldpos = Camera.main.ScreenToWorldPoint(touchpos);
+                    worldpos.z = 0f;
+                    Vector3 point = worldpos - startPosition;
+                    point = startPosition - point;
+                    line.SetPosition(1, point);
+                    direction = new Vector2(point.x - startPosition.x, point.y - startPosition.y);
+                    Debug.Log("Direction: " + direction);
+                }
+                else
+                {
+                    ball.PropellBall(direction.normalized);
+                    inputStarted = false;
+                    onTheWay = true;
+                    line.SetPositions(new Vector3[2] { new Vector3(0, 0, 0), new Vector3(0, 0, 0) });
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/BubbleShooter/Bubble.cs
+++ Assets/Scripts/BubbleShooter/Bubble.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 
@@ -12,8 +15,14 @@ public class Bubble : MonoBehaviour
 
     #region Variable Declarations
     // Serialized Fields
+    [SerializeField] Image image;
     [SerializeField] float power = 5f;
     [SerializeField] Leaf.Color color;
+    [Space]
+    [SerializeField] Color greenColor;
+    [SerializeField] Color blueColor;
+    [SerializeField] Color whiteColor;
+    [SerializeField] Color yellowColor;
     // Private
     private Rigidbody2D rb;
     #endregion
@@ -33,7 +42,7 @@ public class Bubble : MonoBehaviour
     }
     private void Start () 
 	{
-        rb.AddRelativeForce(new Vector2(0, 1) * power, ForceMode2D.Impulse);
+        //rb.AddRelativeForce(new Vector2(0, 1) * power * BubbleShooterManager.instance.canvasTransform.localScale.y, ForceMode2D.Impulse);
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -52,7 +61,32 @@ public class Bubble : MonoBehaviour
 
 
     #region Public Functions
+    public void PropellBall(Vector2 direction)
+    {
+        rb.AddRelativeForce(direction * power * BubbleShooterManager.instance.canvasTransform.localScale.y, ForceMode2D.Impulse);
+    }
 
+    public Bubble CreateNewRandom()
+    {
+        color = (Leaf.Color)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Leaf.Color)).Length);
+        switch (color)
+        {
+            case Leaf.Color.Green:
+                image.color = greenColor;
+                break;
+            case Leaf.Color.Blue:
+                image.color = blueColor;
+                break;
+            case Leaf.Color.White:
+                image.color = whiteColor;
+                break;
+            case Leaf.Color.Yellow:
+                image.color = yellowColor;
+                break;
+            default: break;
+        }
+        return this;
+    }
     #endregion
 
 
