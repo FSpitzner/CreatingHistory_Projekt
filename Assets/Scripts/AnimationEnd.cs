641f1eb24deb3697602a73998bd1abb75af4dc6d
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-11-15 12:28:15 +0100

    Animation shows border

---
+++ Assets/Scripts/AnimationEnd.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEnd : MonoBehaviour {
+
+    public GameObject ui;
+    public GameObject border;
+
+	public void SetAnimationComplete()
+    {
+        ui.SetActive(true);
+        border.SetActive(true);
+    }
+}
