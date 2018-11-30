70de6b92c2ab65cfbe41524e7dd0cd8f09428c9a
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-11-30 11:30:49 +0100

    Aufräumen + Quiz

---
+++ Assets/Scripts/Quiz/Question.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Question")]
+public class Question : ScriptableObject {
+
+    [TextArea(2,4)]
+    public string question;
+    public string answer1;
+    public string answer2;
+    public string answer3;
+    public string answer4;
+    public int rightAnswer;
+
+    public bool CheckAnswer(int playerAnswer)
+    {
+        Debug.Log("Player Answered: " + playerAnswer + " | Right Answer: " + rightAnswer);
+        if (playerAnswer == rightAnswer)
+            return true;
+        return false;
+    }
+
+    public string GetQuestion()
+    {
+        string que;
+        que = question.Replace('#', '\n');
+        return que;
+    }
+}
---
+++ Assets/Scripts/Quiz/QuizController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuizController : MonoBehaviour {
+
+    [Header("Quiz Settings")]
+    public TextMeshProUGUI questionLabel;
+    public TextMeshProUGUI answer1ButtonLabel;
+    public TextMeshProUGUI answer2ButtonLabel;
+    public TextMeshProUGUI answer3ButtonLabel;
+    public TextMeshProUGUI answer4ButtonLabel;
+    public Question[] questions;
+    [Range(0, 1)]
+    public float correctAnswersPercentNeeded;
+    [Space]
+    [Header("General Settings")]
+    public MonologueContent quizWon;
+    public GameEvent quizWonEvent;
+    public MonologueContent quizLost;
+    public GameEvent quizLostEvent;
+    private bool[] answers;
+    private int currentQuestion = 0;
+
+    private void Start()
+    {
+        if (questions.Length != 0)
+        {
+            answers = new bool[questions.Length];
+            Question question = questions[0];
+            questionLabel.text = question.GetQuestion();
+            answer1ButtonLabel.text = question.answer1;
+            answer2ButtonLabel.text = question.answer2;
+            answer3ButtonLabel.text = question.answer3;
+            answer4ButtonLabel.text = question.answer4;
+        }
+    }
+
+    public void NextQuestion()
+    {
+        if(questions.Length <= currentQuestion + 1)
+        {
+            QuizOver();
+            return;
+        }
+        currentQuestion += 1;
+        Question nextQuestion = questions[currentQuestion];
+        questionLabel.text = nextQuestion.GetQuestion();
+        answer1ButtonLabel.text = nextQuestion.answer1;
+        answer2ButtonLabel.text = nextQuestion.answer2;
+        answer3ButtonLabel.text = nextQuestion.answer3;
+        answer4ButtonLabel.text = nextQuestion.answer4;
+    }
+
+    public void AnswerGiven(int buttonNumber)
+    {
+        answers[currentQuestion] = questions[currentQuestion].CheckAnswer(buttonNumber);
+        NextQuestion();
+    }
+
+    private void QuizOver()
+    {
+        int rightAnswers = 0;
+        foreach(bool b in answers)
+        {
+            if (b)
+                rightAnswers += 1;
+        }
+        //Debug.Log("Testing percentage: " + (((float)rightAnswers / (float)(questions.Length))>=correctAnswersPercentNeeded));
+        if((float)rightAnswers/(float)(questions.Length) >= correctAnswersPercentNeeded)
+        {
+            Debug.Log("Won");
+        }
+        else
+        {
+            Debug.Log("Lost");
+        }
+    }
+}
